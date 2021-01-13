bc3737e449e7c897dcac705e1b3415b62ae34536
irina korol <ikorol3107@gmail.com>
2021-01-13 10:50:38 +0100

    refactor(call-context-this): refactor package to typescript
    affects: @lessons/call-context-this
    
    ISSUES CLOSED: #24

---
+++ lessons/call-context-this/src/index.ts
@@ -0,0 +1,60 @@
+/** Normal function: this = window, if 'use strict': this = undefined. */
+function showThis(a: number, b: number): void {
+  console.log(this);
+  function sum(): number {
+    console.log(this);
+    return a + b;
+  }
+
+  console.log(sum());
+}
+showThis(4, 5);
+
+/** The context of object methods is the object itself. */
+const obj: {a :number, b: number, sum: () => void } = {
+  a: 20,
+  b: 15,
+  sum: function (): void {
+    console.log(this);
+  }
+}
+
+/** 'this' in constructors and classes is a new instance of an object. */
+function User(name: string, id: number): void {
+  this.name = name;
+  this.id = id;
+  this.human = true;
+  this.hello = function(): void {
+    console.log("Hello! " + this.name);
+  };
+}
+let ivan = new User('Ivan', 23);
+
+/** Manual binding 'this': call, apply, bind. */
+function sayName(surname: string): void {
+  console.log(this);
+  console.log(this.name + surname);
+}
+
+const myUser: {name: string} = {
+  name: 'John'
+};
+
+sayName.call(myUser, 'Smith');
+sayName.apply(myUser, ['Smith']);
+
+function count(num: number): number {
+  return this*num;
+}
+
+const double: any = count.bind(2);
+console.log(double(3));
+
+/**
+ * The arrow function does not have its own call context, it is taken from the parent.
+ * In event handlers, use 'event.target' instead of 'this'.
+ */
+const btn: any = document.querySelector('button');
+btn.addEventListener('click', (e) => {
+  e.target.style.backgroundColor = 'red';
+});
