4a69fc0bc57d29775c92b7b114348716ec8da5e1
irina korol <ikorol3107@gmail.com>
2021-01-13 11:15:04 +0100

    refactor(how-to-js): refactor package to typescript
    affects: @lessons/how-to-js
    
    ISSUES CLOSED: #24

--- lessons/how-to-js/src/index.js
+++ lessons/how-to-js/src/index.js
@@ -1,18 +1,12 @@
-let numberOfFilms = +prompt( "Сколько фильмов вы уже посмотрели?", "");
-
-const personalMovieDB = {
-    'count': numberOfFilms,
-    'movies': {},
-    'actors': {},
-    'genres': [],
-    'privat': false
+var numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+var personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
-
-let lastFilm1 = prompt("Один из последних просмотренных фильмов?", ""),
-    rate1 = +prompt("На сколько оцените его?", ""),
-    lastFilm2 = prompt("Один из последних просмотренных фильмов?", ""),
-    rate2 = +prompt("На сколько оцените его?", "");
-
+var lastFilm1 = prompt("Один из последних просмотренных фильмов?", ""), rate1 = +prompt("На сколько оцените его?", ""), lastFilm2 = prompt("Один из последних просмотренных фильмов?", ""), rate2 = +prompt("На сколько оцените его?", "");
 personalMovieDB.movies[lastFilm1] = rate1;
 personalMovieDB.movies[lastFilm2] = rate2;
 console.log(personalMovieDB);
