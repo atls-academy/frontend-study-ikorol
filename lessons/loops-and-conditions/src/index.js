644e14868bb0eb521430a76dfffeff7a5f6263d7
irina korol <ikorol3107@gmail.com>
2021-01-13 11:16:54 +0100

    refactor(loops-and-conditions): refactor package to typescript
    affects: @lessons/loops-and-conditions
    
    ISSUES CLOSED: #24

--- lessons/loops-and-conditions/src/index.js
+++ lessons/loops-and-conditions/src/index.js
@@ -1,62 +1,55 @@
-
-let numberOfFilms = +prompt( "Сколько фильмов вы уже посмотрели?", "");
-
-const personalMovieDB = {
-  'count': numberOfFilms,
-  'movies': {},
-  'actors': {},
-  'genres': [],
-  'privat': false
+var numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+var personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
-
 if (personalMovieDB.count < 10) {
-  alert("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-  alert("Вы классический зритель");
-} else if (personalMovieDB.count > 30) {
-  alert("Вы киноман");
-} else {
-  alert("Произошла ошибка");
+    alert("Просмотрено довольно мало фильмов");
 }
-
-// method 1
-for (let i = 0; i < 2; i++) {
-
-  let lastFilm = prompt("Один из последних просмотренных фильмов?", ""),
-    rate = +prompt("На сколько оцените его?", "");
-
-  if (lastFilm !== "" && rate !== 0 && lastFilm != null && rate != null && lastFilm.length < 50 ) {
-    personalMovieDB.movies[lastFilm] = rate;
-  } else {
-    i--;
-  }
+else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    alert("Вы классический зритель");
 }
-
-//method 2
-let a = 0;
-while( a < 2) {
-  let lastFilm = prompt("Один из последних просмотренных фильмов?", ""),
-    rate = +prompt("На сколько оцените его?", "");
-  a++;
-
-  if (lastFilm !== "" && rate !==0 && lastFilm != null && rate != null && lastFilm.length < 50 ) {
-    personalMovieDB.movies[lastFilm] = rate;
-  } else {
-    a--;
-  }
+else if (personalMovieDB.count > 30) {
+    alert("Вы киноман");
 }
-
-//method 3
-let b = 0;
+else {
+    alert("Произошла ошибка");
+}
+for (var i = 0; i < 2; i++) {
+    var lastFilm = prompt("Один из последних просмотренных фильмов?", "");
+    var rate = +prompt("На сколько оцените его?", "");
+    if (lastFilm !== "" && rate !== 0 && lastFilm != null && rate != null && lastFilm.length < 50) {
+        personalMovieDB.movies[lastFilm] = rate;
+    }
+    else {
+        i--;
+    }
+}
+var a = 0;
+while (a < 2) {
+    var lastFilm = prompt("Один из последних просмотренных фильмов?", "");
+    var rate = +prompt("На сколько оцените его?", "");
+    a++;
+    if (lastFilm !== "" && rate !== 0 && lastFilm != null && rate != null && lastFilm.length < 50) {
+        personalMovieDB.movies[lastFilm] = rate;
+    }
+    else {
+        a--;
+    }
+}
+var b = 0;
 do {
-  let lastFilm = prompt("Один из последних просмотренных фильмов?", ""),
-    rate = +prompt("На сколько оцените его?", "");
-  b++;
-  if (lastFilm !== "" && rate !== 0 && lastFilm != null && rate != null && lastFilm.length < 50 ) {
-    personalMovieDB.movies[lastFilm] = rate;
-  } else {
-    b--;
-  }
-} while( b < 2);
-
+    var lastFilm = prompt("Один из последних просмотренных фильмов?", "");
+    var rate = +prompt("На сколько оцените его?", "");
+    b++;
+    if (lastFilm !== "" && rate !== 0 && lastFilm != null && rate != null && lastFilm.length < 50) {
+        personalMovieDB.movies[lastFilm] = rate;
+    }
+    else {
+        b--;
+    }
+} while (b < 2);
 console.log(personalMovieDB);
---
+++ lessons/loops-and-conditions/src/index.ts
@@ -0,0 +1,61 @@
+let numberOfFilms: number = +prompt( "Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB: { count: number, movies: object, actors: object, genres: string[], privat: boolean } = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+
+if (personalMovieDB.count < 10) {
+  alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+  alert("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+  alert("Вы киноман");
+} else {
+  alert("Произошла ошибка");
+}
+
+// method 1
+for (let i: number = 0; i < 2; i++) {
+
+  let lastFilm: string = prompt("Один из последних просмотренных фильмов?", "");
+  let rate: number = +prompt("На сколько оцените его?", "");
+
+  if (lastFilm !== "" && rate !== 0 && lastFilm != null && rate != null && lastFilm.length < 50 ) {
+    personalMovieDB.movies[lastFilm] = rate;
+  } else {
+    i--;
+  }
+}
+
+//method 2
+let a: number = 0;
+while( a < 2) {
+  let lastFilm: string = prompt("Один из последних просмотренных фильмов?", "");
+  let rate: number = +prompt("На сколько оцените его?", "");
+  a++;
+
+  if (lastFilm !== "" && rate !==0 && lastFilm != null && rate != null && lastFilm.length < 50 ) {
+    personalMovieDB.movies[lastFilm] = rate;
+  } else {
+    a--;
+  }
+}
+
+//method 3
+let b: number = 0;
+do {
+  let lastFilm: string = prompt("Один из последних просмотренных фильмов?", "");
+  let rate: number = +prompt("На сколько оцените его?", "");
+  b++;
+  if (lastFilm !== "" && rate !== 0 && lastFilm != null && rate != null && lastFilm.length < 50 ) {
+    personalMovieDB.movies[lastFilm] = rate;
+  } else {
+    b--;
+  }
+} while( b < 2);
+
+console.log(personalMovieDB);
