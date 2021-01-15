6b9e02b6b5aeda6707293779ef4dad17ba421c46
irina korol <ikorol3107@gmail.com>
2021-01-15 16:50:15 +0100

    chore: add ESLint into root directory
    ISSUES CLOSED: #31

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    plugins: [
+        '@typescript-eslint',
+    ],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended'
+    ],
+    env: {
+        node: true,
+        browser: true,
+        jest: true,
+    },
+};
