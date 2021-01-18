c50fa3618f78b194a4946ab88d9091046b1a7875
irina korol <ikorol3107@gmail.com>
2021-01-18 22:23:54 +0100

    chore: integrate ESLint into packages
    affects: @lessons/call-context-this, @lessons/events-and-handlers, @lessons/functions-and-methods,
    @lessons/how-to-js, @lessons/loops-and-conditions, @lessons/objects, @practice/changing-html-via-js,
    @practice/web-page
    
    ISSUES CLOSED: #31

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,35 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    plugins: [
-        '@typescript-eslint',
+    "parser": "@typescript-eslint/parser",
+    "plugins": [
+        "@typescript-eslint"
     ],
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended'
+    "extends": [
+        "airbnb-base",
+        "plugin:prettier/recommended"
     ],
-    env: {
-        node: true,
-        browser: true,
-        jest: true,
+    "env": {
+        "node": false,
+        "browser": true,
+        "es2021": true
     },
+    "rules": {
+        'prettier/prettier': ['error', {
+            semi: false,
+            singleQuote: true,
+            tabWidth: 2,
+            printWidth: 100,
+        }],
+        'class-methods-use-this': 0,
+        'import/no-cycle': 0,
+        'import/no-duplicates': 0,
+        'import/prefer-default-export': 0,
+        'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+        '@typescript-eslint/semi': 0,
+        '@typescript-eslint/no-unused-vars': ['error', { args: 'none' }],
+        'no-useless-constructor': 'off',
+        '@typescript-eslint/no-useless-constructor': 'error',
+        'no-empty-function': 'off',
+        '@typescript-eslint/no-empty-function': 'error',
+        '@typescript-eslint/indent': 0,
+    }
 };
