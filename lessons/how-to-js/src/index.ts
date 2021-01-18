67525eda320458743404b3f878a8734608172541
irina korol <ikorol3107@gmail.com>
2021-01-18 22:37:30 +0100

    chore(how-to-js): change code with ESLint
    affects: @lessons/how-to-js
    
    ISSUES CLOSED: #31

--- lessons/how-to-js/src/index.ts
+++ lessons/how-to-js/src/index.ts
@@ -1,18 +1,24 @@
-let numberOfFilms: number = +prompt( "Сколько фильмов вы уже посмотрели?", "");
+const numberOfFilms: number = +prompt('Сколько·фильмов·вы·уже·посмотрели?', '')
 
-const personalMovieDB: { count: number, movies: object, actors: object, genres: string[], privat: boolean } = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+const personalMovieDB: {
+  count: number
+  movies: object
+  actors: object
+  genres: string[]
+  privat: boolean
+} = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+}
 
-let lastFilm1: string = prompt("Один из последних просмотренных фильмов?", ""),
-    rate1: number = +prompt("На сколько оцените его?", ""),
-    lastFilm2: string = prompt("Один из последних просмотренных фильмов?", ""),
-    rate2: number = +prompt("На сколько оцените его?", "");
+const lastFilm1: string = prompt('Один из последних просмотренных фильмов?', '')
+const rate1: number = +prompt('На сколько оцените его?', '')
+const lastFilm2: string = prompt('Один из последних просмотренных фильмов?', '')
+const rate2: number = +prompt('На сколько оцените его?', '')
 
-personalMovieDB.movies[lastFilm1] = rate1;
-personalMovieDB.movies[lastFilm2] = rate2;
-console.log(personalMovieDB);
+personalMovieDB.movies[lastFilm1] = rate1
+personalMovieDB.movies[lastFilm2] = rate2
+console.log(personalMovieDB)
