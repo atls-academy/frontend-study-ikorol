fd7276838929541a8e2cfdf8ee3761fb5384ae83
irina korol <ikorol3107@gmail.com>
2021-01-19 09:52:26 +0100

    chore: change code with ESLint in packages
    affects: @lessons/call-context-this, @lessons/events-and-handlers, @lessons/objects,
    @practice/changing-html-via-js, @practice/web-page
    
    ISSUES CLOSED: #31

--- lessons/objects/src/index.ts
+++ lessons/objects/src/index.ts
@@ -1,76 +1,81 @@
 const personalMovieDB: {
-  count: number,
-  movies: object,
-  actors: object,
-  genres: string[],
-  private: boolean,
-  start: () => void,
-  detectPersonalLevel: () => void,
-  writeYourGenres: () => void,
-  rememberMyFilms: () => void,
-  toggleVisibleMyDB: () => void,
+  count: number
+  movies: object
+  actors: object
+  genres: string[]
+  private: boolean
+  start: () => void
+  detectPersonalLevel: () => void
+  writeYourGenres: () => void
+  rememberMyFilms: () => void
+  toggleVisibleMyDB: () => void
   showMyDB: () => void
-  } = {
+} = {
   count: 0,
   movies: {},
   actors: {},
   genres: [],
   private: false,
-  start: function(): void {
-    this.count = prompt( "Сколько фильмов вы уже посмотрели?", "");
-    while (this.count === '' || this.count == null || isNaN(this.count)) {
-      this.count = prompt( "Сколько фильмов вы уже посмотрели?", "");
+  start(): void {
+    this.count = prompt('Сколько фильмов вы уже посмотрели?', '')
+    while (this.count === '' || this.count == null || Number.isNaN(this.count)) {
+      this.count = prompt('Сколько фильмов вы уже посмотрели?', '')
     }
   },
-  detectPersonalLevel: function(): void {
+  detectPersonalLevel(): void {
     if (personalMovieDB.count < 10) {
-      alert("Просмотрено довольно мало фильмов");
+      alert('Просмотрено довольно мало фильмов')
     } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-      alert("Вы классический зритель");
+      alert('Вы классический зритель')
     } else if (personalMovieDB.count > 30) {
-      alert("Вы киноман");
+      alert('Вы киноман')
     } else {
-      alert("Произошла ошибка");
+      alert('Произошла ошибка')
     }
   },
-  writeYourGenres: function(): void {
-    for (let i: number = 0; i < 3; i++ ) {
-      let personalGenre: string = prompt( `Ваш любимый жанр под номером ${i + 1}`, "");
+  writeYourGenres(): void {
+    for (let i: number = 0; i < 3; i++) {
+      let personalGenre: string = prompt(`Ваш любимый жанр под номером ${i + 1}`, '')
       while (personalGenre === '' || personalGenre == null) {
-        personalGenre = prompt( `Ваш любимый жанр под номером ${i + 1}`, "");
+        personalGenre = prompt(`Ваш любимый жанр под номером ${i + 1}`, '')
       }
-      this.genres[ i ] = personalGenre;
+      this.genres[i] = personalGenre
     }
-    this.genres.forEach( (item: string, i: number) => {
-      console.log( `Любимый жанр ${ i + 1 } - это ${ item }`);
-    });
+    this.genres.forEach((item: string, i: number) => {
+      console.log(`Любимый жанр ${i + 1} - это ${item}`)
+    })
   },
-  rememberMyFilms: function(): void {
+  rememberMyFilms(): void {
     for (let i: number = 0; i < 2; i++) {
-      let lastFilm: string = prompt("Один из последних просмотренных фильмов?", "");
-      let rate: number = +prompt("На сколько оцените его?", "");
+      const lastFilm: string = prompt('Один из последних просмотренных фильмов?', '')
+      const rate: number = +prompt('На сколько оцените его?', '')
 
-      if (lastFilm !== "" && rate !== 0 && lastFilm != null && rate != null && lastFilm.length < 50 ) {
-        personalMovieDB.movies[lastFilm] = rate;
+      if (
+        lastFilm !== '' &&
+        rate !== 0 &&
+        lastFilm != null &&
+        rate != null &&
+        lastFilm.length < 50
+      ) {
+        personalMovieDB.movies[lastFilm] = rate
       } else {
-        i--;
+        i--
       }
     }
   },
-  toggleVisibleMyDB: function(): void {
-    this.private = !this.private;
+  toggleVisibleMyDB(): void {
+    this.private = !this.private
   },
-  showMyDB: function(): void {
-    if ( !this.private ) {
-      console.log( personalMovieDB );
+  showMyDB(): void {
+    if (!this.private) {
+      console.log(personalMovieDB)
     }
-  }
+  },
+}
 
-};
-
-personalMovieDB.start();
-personalMovieDB.detectPersonalLevel();
-personalMovieDB.writeYourGenres();
-personalMovieDB.rememberMyFilms();
-personalMovieDB.toggleVisibleMyDB();
-personalMovieDB.showMyDB();
+personalMovieDB.start()
+personalMovieDB.detectPersonalLevel()
+personalMovieDB.writeYourGenres()
+personalMovieDB.rememberMyFilms()
+personalMovieDB.toggleVisibleMyDB()
+personalMovieDB.showMyDB()
--- practice/web-page/src/ts/index.ts
+++ practice/web-page/src/ts/index.ts
@@ -1,145 +1,148 @@
 window.addEventListener('DOMContentLoaded', () => {
+  // Tabs
 
-  //Tabs
-
-  const tabs = document.querySelectorAll('.tabheader__item');
-  const tabsContent = document.querySelectorAll('.tabcontent');
-  const tabsParent = document.querySelector('.tabheader__items');
+  const tabs = document.querySelectorAll('.tabheader__item')
+  const tabsContent = document.querySelectorAll('.tabcontent')
+  const tabsParent = document.querySelector('.tabheader__items')
 
   function hideTabContent(): void {
-    tabsContent.forEach(item => {
-      item.classList.add('hide');
-      item.classList.remove('show', 'fade');
-    });
+    tabsContent.forEach((item) => {
+      item.classList.add('hide')
+      item.classList.remove('show', 'fade')
+    })
 
-    tabs.forEach(item => {
-      item.classList.remove('tabheader__item_active');
+    tabs.forEach((item) => {
+      item.classList.remove('tabheader__item_active')
     })
   }
 
   function showTabContent(i: number = 0): void {
-    tabsContent[i].classList.add('show', 'fade');
-    tabsContent[i].classList.remove('hide');
-    tabs[i].classList.add('tabheader__item_active');
+    tabsContent[i].classList.add('show', 'fade')
+    tabsContent[i].classList.remove('hide')
+    tabs[i].classList.add('tabheader__item_active')
   }
 
-  hideTabContent();
-  showTabContent();
+  hideTabContent()
+  showTabContent()
 
   function hasClass(element: any, className: string): boolean {
-    return (element.className).indexOf(className) > -1;
+    return element.className.indexOf(className) > -1
   }
 
   tabsParent.addEventListener('click', (e) => {
-    const target = e.target;
+    const { target } = e
 
     if (target && hasClass(target, 'tabheader__item')) {
       tabs.forEach((item, i) => {
         if (target === item) {
-          hideTabContent();
-          showTabContent(i);
+          hideTabContent()
+          showTabContent(i)
         }
       })
     }
   })
 
-  //Countdown timer
+  // Countdown timer
 
-  const deadline: string = '2020-12-31';
+  const deadline: string = '2020-12-31'
 
-  function getTimeRemaining(endTime: string): { total: number, days: number, hours: number, minutes: number, seconds: number } {
-    const t: number = Date.parse(endTime) - Date.parse(new Date().toISOString());
-    const days: number = Math.floor(t / (1000 * 60 * 60 * 24));
-    const hours: number = Math.floor((t / (1000 * 60 * 60) %  24));
-    const minutes: number = Math.floor((t / 1000 / 60)  %  60);
-    const seconds: number = Math.floor((t / 1000)  %  60);
+  function getTimeRemaining(
+    endTime: string
+  ): { total: number; days: number; hours: number; minutes: number; seconds: number } {
+    const t: number = Date.parse(endTime) - Date.parse(new Date().toISOString())
+    const days: number = Math.floor(t / (1000 * 60 * 60 * 24))
+    const hours: number = Math.floor((t / (1000 * 60 * 60)) % 24)
+    const minutes: number = Math.floor((t / 1000 / 60) % 60)
+    const seconds: number = Math.floor((t / 1000) % 60)
 
     return {
       total: t,
-      days: days,
-      hours: hours,
-      minutes: minutes,
-      seconds: seconds
-    };
+      days,
+      hours,
+      minutes,
+      seconds,
+    }
   }
 
   function getZero(num: number): string | number {
     if (num >= 0 && num < 10) {
-      return `0${num}`;
-    } else {
-      return num;
+      return `0${num}`
     }
+    return num
   }
 
   function setClock(selector: any, endTime: string): void {
-    const timer = document.querySelector(selector);
-    const days = timer.querySelector('#days');
-    const hours = timer.querySelector('#hours');
-    const minutes = timer.querySelector('#minutes');
-    const seconds = timer.querySelector('#seconds');
-    const timeInterval = setInterval(updateClock, 1000);
+    const timer = document.querySelector(selector)
+    const days = timer.querySelector('#days')
+    const hours = timer.querySelector('#hours')
+    const minutes = timer.querySelector('#minutes')
+    const seconds = timer.querySelector('#seconds')
+    const timeInterval = setInterval(updateClock, 1000)
 
-    updateClock();
+    updateClock()
 
     function updateClock(): void {
-      const t = getTimeRemaining(endTime);
+      const t = getTimeRemaining(endTime)
 
-      days.innerHTML = getZero(t.days);
-      hours.innerHTML = getZero(t.hours);
-      minutes.innerHTML = getZero(t.minutes);
-      seconds.innerHTML = getZero(t.seconds);
+      days.innerHTML = getZero(t.days)
+      hours.innerHTML = getZero(t.hours)
+      minutes.innerHTML = getZero(t.minutes)
+      seconds.innerHTML = getZero(t.seconds)
 
-      if(t.total <= 0) {
-        clearInterval(timeInterval);
+      if (t.total <= 0) {
+        clearInterval(timeInterval)
       }
     }
   }
 
-  setClock('.timer', deadline);
+  setClock('.timer', deadline)
 
   // Modal window
 
-  const modalTrigger = document.querySelectorAll('[data-modal]');
-  const modalWindow = document.querySelector('.modal');
-  const modalClose = document.querySelector('[data-close]');
+  const modalTrigger = document.querySelectorAll('[data-modal]')
+  const modalWindow = document.querySelector('.modal')
+  const modalClose = document.querySelector('[data-close]')
 
   function openModalWindow(): void {
-    modalWindow.classList.toggle('show');
-    document.body.style.overflow = 'hidden';
-    clearInterval(modalTimer);
+    modalWindow.classList.toggle('show')
+    document.body.style.overflow = 'hidden'
+    clearInterval(modalTimer)
   }
 
-  modalTrigger.forEach(btn => {
-    btn.addEventListener('click', openModalWindow);
-  });
+  modalTrigger.forEach((btn) => {
+    btn.addEventListener('click', openModalWindow)
+  })
 
   function closeModalWindow(): void {
-    modalWindow.classList.toggle('show');
-    document.body.style.overflow = '';
+    modalWindow.classList.toggle('show')
+    document.body.style.overflow = ''
   }
 
-  modalClose.addEventListener('click', closeModalWindow);
+  modalClose.addEventListener('click', closeModalWindow)
 
   modalWindow.addEventListener('click', (e) => {
     if (e.target === modalWindow) {
-      closeModalWindow();
+      closeModalWindow()
     }
-  });
+  })
 
   document.addEventListener('keydown', (e) => {
-    if (e.code === "Escape" && modalWindow.classList.contains('show')) {
-      closeModalWindow();
+    if (e.code === 'Escape' && modalWindow.classList.contains('show')) {
+      closeModalWindow()
     }
-  });
+  })
 
-  const modalTimer = setTimeout(openModalWindow, 15000);
+  const modalTimer = setTimeout(openModalWindow, 15000)
 
   function showModalByScroll(): void {
-    if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
-      openModalWindow();
-      window.removeEventListener('scroll', showModalByScroll);
+    if (
+      window.pageYOffset + document.documentElement.clientHeight >=
+      document.documentElement.scrollHeight
+    ) {
+      openModalWindow()
+      window.removeEventListener('scroll', showModalByScroll)
     }
   }
 
-  window.addEventListener('scroll', showModalByScroll);
+  window.addEventListener('scroll', showModalByScroll)
 })
