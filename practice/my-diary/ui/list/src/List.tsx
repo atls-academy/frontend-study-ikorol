d00400cdda4b267bb25041cc80c9649f45004986
irina korol <ikorol3107@gmail.com>
2021-04-22 09:54:29 +0200

    refactor(my-diary): add layout to list note
    affects: @practice/my-diary, @ui/list
    
    ISSUES CLOSED: #68

--- practice/my-diary/ui/list/src/List.tsx
+++ practice/my-diary/ui/list/src/List.tsx
@@ -11,43 +11,44 @@ import { Item }                           from './Item'
 export const List: any = ({ deleteItem, toggleStatus }) => {
   const [notes, setNotes] = useNotes()
 
-  const elements = notes.map(item => {
-    return (
-      <Column key={item.id}>
-        <Row>
-          <Item onDoubleClick={() => setNotes(toggleStatus(notes, item.id, 'liked'))}>
-            <Text lineHeight='40px' padding='0 6px'>
-              {item.note}
-            </Text>
-          </Item>
-          <Layout flexBasis={3} />
-          <Box>
-            <Button
-              backgroundColor='#fff'
-              border='none'
-              width='40px'
-              onClick={() => setNotes(toggleStatus(notes, item.id, 'important'))}
-            >
-              <StarIcon color={item.important ? '#FFD700' : '#6c757d'} />
-            </Button>
+  return (
+    <Column>
+      {notes.map(item => (
+        <>
+          <Row key={item.id}>
+            <Item onDoubleClick={() => setNotes(toggleStatus(notes, item.id, 'liked'))}>
+              <Text lineHeight='40px' padding='0 6px'>
+                {item.note}
+              </Text>
+            </Item>
             <Layout flexBasis={3} />
-            <Button
-              backgroundColor='#fff'
-              border='none'
-              width='40px'
-              onClick={() => setNotes(deleteItem(notes, item.id))}
-            >
-              <TrashIcon />
-            </Button>
-            <Layout flexBasis={3} />
-            <Button backgroundColor='#fff' border='none' width='40px'>
-              <HeartIcon opacity={item.liked ? '1' : '0'} />
-            </Button>
-          </Box>
-        </Row>
-        <Layout flexBasis={5} />
-      </Column>
-    )
-  })
-  return elements
+            <Box>
+              <Button
+                backgroundColor='#fff'
+                border='none'
+                width='40px'
+                onClick={() => setNotes(toggleStatus(notes, item.id, 'important'))}
+              >
+                <StarIcon color={item.important ? '#FFD700' : '#6c757d'} />
+              </Button>
+              <Layout flexBasis={3} />
+              <Button
+                backgroundColor='#fff'
+                border='none'
+                width='40px'
+                onClick={() => setNotes(deleteItem(notes, item.id))}
+              >
+                <TrashIcon />
+              </Button>
+              <Layout flexBasis={3} />
+              <Button backgroundColor='#fff' border='none' width='40px'>
+                <HeartIcon opacity={item.liked ? '1' : '0'} />
+              </Button>
+            </Box>
+          </Row>
+          <Layout flexBasis={6} />
+        </>
+      ))}
+    </Column>
+  )
 }
