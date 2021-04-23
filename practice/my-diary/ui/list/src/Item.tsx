6060cd78c58cbccc9080785febc3cb66d1a1aa6e
irina korol <ikorol3107@gmail.com>
2021-04-23 09:41:25 +0200

    refactor(my-diary): remake item component
    affects: @practice/my-diary, @ui/list
    
    ISSUES CLOSED: #68

--- practice/my-diary/ui/list/src/Item.tsx
+++ practice/my-diary/ui/list/src/Item.tsx
@@ -1,26 +1,61 @@
-import styled                                                        from '@emotion/styled'
-import { border, color, flexbox, layout, space, system, typography } from 'styled-system'
+import React                              from 'react'
+import styled                             from '@emotion/styled'
+import { border, color }                  from 'styled-system'
 
-export const Item = styled.li(
-  system({
-    boxSizing: true,
-    overflowX: true,
-    overflowY: true,
+import { Button }                         from '@ui/button'
+import { HeartIcon, StarIcon, TrashIcon } from '@ui/icons'
+import { Box, Layout, Row }               from '@ui/layout'
+import { Text }                           from '@ui/text'
+import { useNotes }                       from '@store/notes'
+
+const StyledItem = styled.li(
+  ({ ...props }) => ({
+    display: 'flex',
+    boxSizing: 'border-box',
+    width: '100%',
+    borderRadius: '4px',
+    backgroundColor: '#fff',
   }),
-  typography,
-  flexbox,
-  layout,
   color,
-  space,
   border,
 )
 
-Item.defaultProps = {
-  fontSize: '20px',
-  boxSizing: 'border-box',
-  justifyContent: 'space-between',
-  display: 'flex',
-  width: '100%',
-  backgroundColor: '#fff',
-  borderRadius: '4px',
+export const Item = ({ note, deleteItem, toggleStatus }) => {
+  const [notes, setNotes] = useNotes()
+  return (
+    <>
+      <Row>
+        <StyledItem onDoubleClick={() => setNotes(toggleStatus(notes, note.id, 'liked'))}>
+          <Text lineHeight='40px' padding='0 6px'>
+            {note.note}
+          </Text>
+        </StyledItem>
+        <Layout flexBasis={3} />
+        <Box>
+          <Button
+            backgroundColor='#fff'
+            border='none'
+            width='40px'
+            onClick={() => setNotes(toggleStatus(notes, note.id, 'important'))}
+          >
+            <StarIcon color={note.important ? '#FFD700' : '#6c757d'} />
+          </Button>
+          <Layout flexBasis={3} />
+          <Button
+            backgroundColor='#fff'
+            border='none'
+            width='40px'
+            onClick={() => setNotes(deleteItem(notes, note.id))}
+          >
+            <TrashIcon />
+          </Button>
+          <Layout flexBasis={3} />
+          <Button backgroundColor='#fff' border='none' width='40px'>
+            <HeartIcon opacity={note.liked ? '1' : '0'} />
+          </Button>
+        </Box>
+      </Row>
+      <Layout flexBasis={6} />
+    </>
+  )
 }
--- practice/my-diary/ui/list/src/List.tsx
+++ practice/my-diary/ui/list/src/List.tsx
@@ -1,53 +1,17 @@
-import React                              from 'react'
+import React        from 'react'
 
-import { Button }                         from '@ui/button'
-import { HeartIcon, StarIcon, TrashIcon } from '@ui/icons'
-import { Box, Column, Layout, Row }       from '@ui/layout'
-import { Text }                           from '@ui/text'
-import { useNotes }                       from '@store/notes'
+import { Column }   from '@ui/layout'
+import { useNotes } from '@store/notes'
 
-import { Item }                           from './Item'
+import { Item }     from './Item'
 
 export const List: any = ({ deleteItem, toggleStatus }) => {
-  const [notes, setNotes] = useNotes()
+  const [notes] = useNotes()
 
   return (
     <Column>
       {notes.map(item => (
-        <>
-          <Row key={item.id}>
-            <Item onDoubleClick={() => setNotes(toggleStatus(notes, item.id, 'liked'))}>
-              <Text lineHeight='40px' padding='0 6px'>
-                {item.note}
-              </Text>
-            </Item>
-            <Layout flexBasis={3} />
-            <Box>
-              <Button
-                backgroundColor='#fff'
-                border='none'
-                width='40px'
-                onClick={() => setNotes(toggleStatus(notes, item.id, 'important'))}
-              >
-                <StarIcon color={item.important ? '#FFD700' : '#6c757d'} />
-              </Button>
-              <Layout flexBasis={3} />
-              <Button
-                backgroundColor='#fff'
-                border='none'
-                width='40px'
-                onClick={() => setNotes(deleteItem(notes, item.id))}
-              >
-                <TrashIcon />
-              </Button>
-              <Layout flexBasis={3} />
-              <Button backgroundColor='#fff' border='none' width='40px'>
-                <HeartIcon opacity={item.liked ? '1' : '0'} />
-              </Button>
-            </Box>
-          </Row>
-          <Layout flexBasis={6} />
-        </>
+        <Item note={item} key={item.id} deleteItem={deleteItem} toggleStatus={toggleStatus} />
       ))}
     </Column>
   )
