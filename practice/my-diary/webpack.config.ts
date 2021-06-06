064269474617b5d8bd96eba899b508b779a029cb
irina korol <ikorol3107@gmail.com>
2021-06-06 07:38:52 +0200

    refactor(my-diaty): add meta tag viewport
    affects: @practice/my-diary
    
    ISSUES CLOSED: #78

--- practice/my-diary/webpack.config.ts
+++ practice/my-diary/webpack.config.ts
@@ -16,6 +16,13 @@ export const plugins = [
     appMountId: 'root',
     title: 'My diary',
     scripts: ['bundle.js'],
+    // @ts-ignore
+    meta: [
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1, shrink-to-fit=no',
+      },
+    ],
   }),
 ]
 
